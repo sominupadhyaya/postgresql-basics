47374c8097c04b0bb45f9a93074ed2c595207b19
Somin <sominupadhyaya@gmail.com>
2022-08-03 09:15:02 +0545

    insert values into users

--- table.postgres.sql
+++ table.postgres.sql
@@ -1,13 +1,13 @@
--- creating a table 
+-- CREATING A TABLE
 
 
-create table users (
-    id serial primary key, 
-    first_name varchar(255) not null,
-    last_name text,
-    age int,
-    email text unique not null 
-);
+-- create table users (
+--     id serial primary key, 
+--     first_name varchar(255) not null,
+--     last_name text,
+--     age int,
+--     email text unique not null 
+-- );
 
 /*
 -> 'serial' means that we dont have to
@@ -16,4 +16,47 @@ automatically.
 
 -> 'unique' means that there mustnt be
 any duplications of a value  
+*/
+
+
+
+-- INSERTING DATA INTO TABLE
+
+/* you can insert multiple datas into 
+the table by using  */
+
+insert into users(
+    first_name,
+    last_name,
+    age,
+    email
+)
+values(
+    'person1',
+    'person',
+    18,
+    'person1@gmail.com'
+)
+,(
+    'person2',
+    'person',
+    22,
+    'person2@gmail.com'
+)
+,(
+    'peter',
+    'smith',
+    25,
+    'peter@gmail.com'
+)
+,(
+    'john',
+    'doe',
+    27,
+    'john@gmail.com'
+);
+
+
+/* this is how to insert multiple
+rows at once. Here, we just inserted 4 rows at once
 */
