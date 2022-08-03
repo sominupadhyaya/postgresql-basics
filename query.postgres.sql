4d6d309f5682fc537bc971fe5537d32bfec23136
Somin <sominupadhyaya@gmail.com>
2022-08-03 09:23:16 +0545

    complete select basic example

---
+++ query.postgres.sql
@@ -0,0 +1,22 @@
+--check the table.postgres file first
+
+--SELECT COMMAND
+
+/*
+It helps us select data from a table or database
+*/
+
+-- to select everything in a table use:
+
+select * from users
+
+/*
+the * means all so it will return 
+every row that is:  
+1	person1	person	18	person1@gmail.com
+2	person2	person	22	person2@gmail.com
+3	peter	smith	25	peter@gmail.com
+4	john	doe	    27	john@gmail.com
+*/
+
+
--- table.postgres.sql
+++ table.postgres.sql
@@ -25,38 +25,41 @@ any duplications of a value
 /* you can insert multiple datas into 
 the table by using  */
 
-insert into users(
-    first_name,
-    last_name,
-    age,
-    email
-)
-values(
-    'person1',
-    'person',
-    18,
-    'person1@gmail.com'
-)
-,(
-    'person2',
-    'person',
-    22,
-    'person2@gmail.com'
-)
-,(
-    'peter',
-    'smith',
-    25,
-    'peter@gmail.com'
-)
-,(
-    'john',
-    'doe',
-    27,
-    'john@gmail.com'
-);
+-- insert into users(
+--     first_name,
+--     last_name,
+--     age,
+--     email
+-- )
+-- values(
+--     'person1',
+--     'person',
+--     18,
+--     'person1@gmail.com'
+-- )
+-- ,(
+--     'person2',
+--     'person',
+--     22,
+--     'person2@gmail.com'
+-- )
+-- ,(
+--     'peter',
+--     'smith',
+--     25,
+--     'peter@gmail.com'
+-- )
+-- ,(
+--     'john',
+--     'doe',
+--     27,
+--     'john@gmail.com'
+-- );
 
 
 /* this is how to insert multiple
 rows at once. Here, we just inserted 4 rows at once
 */
+
+
+--check the query.postgres file from now 
