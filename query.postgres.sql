43f2f68781d702dcf94d2ca894f01ebd46469099
Somin <sominupadhyaya@gmail.com>
2022-08-03 09:41:47 +0545

    complete basic select commands

--- query.postgres.sql
+++ query.postgres.sql
@@ -8,7 +8,7 @@ It helps us select data from a table or database
 
 -- to select everything in a table use:
 
-select * from users
+-- select * from users
 
 /*
 the * means all so it will return 
@@ -19,4 +19,27 @@ every row that is:
 4	john	doe	    27	john@gmail.com
 */
 
+-- SELECTING  SPECIFIC ROW
 
+-- select * from users
+-- where id = 3;
+
+-- which returns : 
+-- 3	peter	smith	25	peter@gmail.com
+
+-- SELECTING MULTIPLE ROWS WITH OR 
+
+-- select * from users
+-- where id = 3 or id = 4
+
+-- which returns :
+-- 3	peter	smith	25	peter@gmail.com
+-- 2	john	doe	27	john@gmail.com
+
+-- SELECTING SAME ROW WITH MULTIPLE ATTRIBUTES
+
+select * from users
+where first_name = 'person1' and id = 1
+
+-- which returns :
+-- person1	person	18	person1@gmail.com
