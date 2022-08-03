e45e747a97c693a2500e3765d159285bc69ceb52
Somin <sominupadhyaya@gmail.com>
2022-08-03 09:01:53 +0545

    create table users

--- table.postgres.sql
+++ table.postgres.sql
@@ -1,1 +1,19 @@
+-- creating a table 
 
+
+create table users (
+    id serial primary key, 
+    first_name varchar(255) not null,
+    last_name text,
+    age int,
+    email text unique not null 
+);
+
+/*
+-> 'serial' means that we dont have to
+insert the value. It will be there serially
+automatically.
+
+-> 'unique' means that there mustnt be
+any duplications of a value  
+*/
