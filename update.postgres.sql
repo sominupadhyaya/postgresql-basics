e36203657e3ba19bfde01e106ebd92ade75f4e29
Somin <sominupadhyaya@gmail.com>
2022-08-04 09:07:20 +0545

    change file names

---
+++ update.postgres.sql
@@ -0,0 +1,17 @@
+-- UPDATE COMMAND 
+
+-- used to change or update the value in a db
+
+--CHANGING THE VALUE OF ROW 
+
+-- update users 
+-- set age = 20
+-- where id = 1;
+
+-- select * from users where id = 1;
+
+-- returns : 
+-- 1	person1	person	20	person1@gmail.com
+
+
+
