bd92b368d7ace06f891b07af6e4458be943d80b3
Somin <sominupadhyaya@gmail.com>
2022-08-04 09:20:57 +0545

    complete update commands

--- update.postgres.sql
+++ update.postgres.sql
@@ -8,10 +8,28 @@
 -- set age = 20
 -- where id = 1;
 
--- select * from users where id = 1;
-
 -- returns : 
 -- 1	person1	person	20	person1@gmail.com
 
+-- CHANGING MULTIPLE VALUES IN ROWS
+-- update users
+-- set age = 23 ,
+-- first_name = 'someone',
+-- email = 'someone@gmail.com'
+-- where id = 1;
+
+--returns :
+-- 1 someone person 23 someone@gmail.com 
+
+
+--SETTING SOMETHING FOR EVERY ROW
+
+-- update users set age = age+1;
+-- select * from users
+
+--increments age for everyone by 1 
+
+--note : you can perform the any of the where condition
+-- mentioned in the file before for updating as well
 
 
