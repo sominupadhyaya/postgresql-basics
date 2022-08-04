37b1759b8bb0c8ee4aef4b90408f42f12d1adfdc
Somin <sominupadhyaya@gmail.com>
2022-08-04 09:34:08 +0545

    complete basic delete syntax

---
+++ delete.postgres.sql
@@ -0,0 +1,21 @@
+-- DELETE COMMAND
+
+-- FOR DELETING SPECIFIC ROWS:
+
+-- delete from users
+-- where id = 2;
+
+/*before
+2	person2	person	23	person2@gmail.com
+3	peter	smith	26	peter@gmail.com
+4	john	doe	    28	john@gmail.com
+1	someone	person	24	someone@gmail.com
+*/
+
+/*after query
+3	peter	smith	26	peter@gmail.com
+4	john	doe	    28	john@gmail.com
+1	someone	person	24	someone@gmail.com
+*/
+
+
